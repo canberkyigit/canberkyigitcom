7842042eba7b75a4b70cc07e42596962c6e24244
Canberk Yigit <canberkyigit1996@gmail.com>
2024-05-21 16:39:43 +0300

    Updates

---
+++ precache-manifest.bb9a72ab26d1acf6a207e6dd91fca0c4.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94021af349faa03ccf24222d7617370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95a2ba7bc4bde9a7d33c",
+    "url": "/static/css/2.5b0a96a7.chunk.css"
+  },
+  {
+    "revision": "4794b2689aeef7c92b9e",
+    "url": "/static/css/main.1998ef85.chunk.css"
+  },
+  {
+    "revision": "95a2ba7bc4bde9a7d33c",
+    "url": "/static/js/2.47ebf7e4.chunk.js"
+  },
+  {
+    "revision": "e2ebd2f00eaa62174a05",
+    "url": "/static/js/3.05cd8c2e.chunk.js"
+  },
+  {
+    "revision": "4794b2689aeef7c92b9e",
+    "url": "/static/js/main.13a12160.chunk.js"
+  },
+  {
+    "revision": "e15c04315a7adc93d817",
+    "url": "/static/js/runtime~main.99f1f87e.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "497ae1d359cfc7fc7f5238cf8eb69dc1",
+    "url": "/static/media/axalta.497ae1d3.jpg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "3b73ace0e70a88c685eda639f79625f0",
+    "url": "/static/media/danone.3b73ace0.jpg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "540c929387da93b0f04e033b096b75cb",
+    "url": "/static/media/jnj.540c9293.jpg"
+  },
+  {
+    "revision": "cc75be8ee4c4fe18eb47353965966719",
+    "url": "/static/media/lifescan.cc75be8e.jpg"
+  },
+  {
+    "revision": "7024d3593f549395c40f92fb4db6f241",
+    "url": "/static/media/logo.7024d359.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "8b24d58c6d4857737d993b0d85044526",
+    "url": "/static/media/nav-icon1.8b24d58c.svg"
+  },
+  {
+    "revision": "f762bd72585b639a1d4c3b706c8c415c",
+    "url": "/static/media/nav-icon2.f762bd72.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "ebe3baa623a4f0f0d9878fcbf04ed264",
+    "url": "/static/media/nestle.ebe3baa6.jpg"
+  },
+  {
+    "revision": "64341e48815a38b556f4ae50dd770f05",
+    "url": "/static/media/png.64341e48.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
